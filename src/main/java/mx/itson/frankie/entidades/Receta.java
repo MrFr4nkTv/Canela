abfffcf67647d1cdefc652ea421fb9b6a7fdab39
alumnog <alumnog@LG902_24.itson.edu.mx>
2022-09-27 08:53:49 -0700

    Version Inicial del Proyecto

---
+++ src/main/java/mx/itson/frankie/entidades/Receta.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.frankie.entidades;
+
+import java.util.List;
+import mx.itson.frankie.enumeradores.Dificultad;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Receta {
+    private String nombre;
+    private String descrpcion;
+    private int numeroPorciones;
+    private int duracion;
+    private List<Ingrediente> ingredientes;
+    private List<Paso> pasos;
+    private Dificultad dificultad;
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descrpcion
+     */
+    public String getDescrpcion() {
+        return descrpcion;
+    }
+
+    /**
+     * @param descrpcion the descrpcion to set
+     */
+    public void setDescrpcion(String descrpcion) {
+        this.descrpcion = descrpcion;
+    }
+
+    /**
+     * @return the numeroPorciones
+     */
+    public int getNumeroPorciones() {
+        return numeroPorciones;
+    }
+
+    /**
+     * @param numeroPorciones the numeroPorciones to set
+     */
+    public void setNumeroPorciones(int numeroPorciones) {
+        this.numeroPorciones = numeroPorciones;
+    }
+
+    /**
+     * @return the duracion
+     */
+    public int getDuracion() {
+        return duracion;
+    }
+
+    /**
+     * @param duracion the duracion to set
+     */
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    /**
+     * @return the ingredientes
+     */
+    public List<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    /**
+     * @param ingredientes the ingredientes to set
+     */
+    public void setIngredientes(List<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    /**
+     * @return the pasos
+     */
+    public List<Paso> getPasos() {
+        return pasos;
+    }
+
+    /**
+     * @param pasos the pasos to set
+     */
+    public void setPasos(List<Paso> pasos) {
+        this.pasos = pasos;
+    }
+
+    /**
+     * @return the dificultad
+     */
+    public Dificultad getDificultad() {
+        return dificultad;
+    }
+
+    /**
+     * @param dificultad the dificultad to set
+     */
+    public void setDificultad(Dificultad dificultad) {
+        this.dificultad = dificultad;
+    }
+    
+    
+}
